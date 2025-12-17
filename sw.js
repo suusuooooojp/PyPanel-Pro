2d1106203ff3a4497c424d178a2f3223e1b81919
suusuo <rdgdtrdr@gmail.com>
2025-12-18 08:30:47 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pypanel-shell-v5';
+const CACHE_NAME = 'pypanel-bundler-v1';
 const ASSETS = [
     './',
     './index.html',
@@ -6,11 +6,5 @@ const ASSETS = [
     './py-worker.js',
     'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.45.0/min/vs/loader.js'
 ];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
+self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))); });
